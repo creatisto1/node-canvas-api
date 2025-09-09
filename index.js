46f5c327dbb5580bc42d557b66b711d4c1164775
creatisto1 <felix.pfeffer@creatisto.com>
2025-09-09 12:33:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const verspielto = require('./templates/04_Verspielto');
 const standardo = require('./templates/05_Standardo');
 const quadrat = require('./templatesCrop/06_quadrat');
 const cta_2 = require('./templates/07_CTA_2');
+const cta_2_inverted = require('./templates/07_CTA_2_inverted');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -470,6 +471,45 @@ app.post('/cta_2', async (req, res) => {
 
 
     const filename = `img-cta_2-${Date.now()}.png`;
+    const savePath = path.join(publicDir, filename);
+    const out = fs.createWriteStream(savePath);
+    const stream = canvas.createPNGStream();
+
+    stream.pipe(out);
+    out.on('finish', () => {
+      const imgUrl = `${req.protocol}://${req.get('host')}/public/${filename}`;
+      res.json({ imgUrl });
+    });
+
+     } catch (error) {
+    console.error('Fehler:', error);
+    res.status(500).send('Fehler beim Verarbeiten des Bildes');
+  }
+});
+
+// Neue Route: CTA_2_inverted Template
+app.post('/cta_2_inverted', async (req, res) => {
+  const imageUrl = req.body.url;
+  const website = req.body.website || null;
+  let overlayText = req.body.overlay || 'Hello, World!';
+  overlayText = overlayText.toUpperCase();
+
+  console.log('Empfangene Website:', website);
+
+  if (!imageUrl) {
+    return res.status(400).send('Missing "url" in request body');
+  }
+
+  try {
+    const img = await loadImage(imageUrl);
+    const targetWidth = img.width;
+    const targetHeight = img.height;
+
+    const quadCanvas = await quadrat(img);
+    const canvas = await cta_2_inverted(quadCanvas, overlayText, website);
+
+
+    const filename = `img-cta_2_inverted-${Date.now()}.png`;
     const savePath = path.join(publicDir, filename);
     const out = fs.createWriteStream(savePath);
     const stream = canvas.createPNGStream();
